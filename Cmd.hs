9f062ca03a26632064db8908f929fb3e6a3a1806
즈눅 <admin@xnu.kr>
2015-09-23 14:48:33 +0900

    EMOJIS

--- Cmd.hs
+++ Cmd.hs
@@ -6,6 +6,7 @@ import qualified PluginDaumDic as DaumDic
 import qualified Data.ByteString as BS hiding (drop)
 import qualified Data.ByteString.UTF8 as BS
 import qualified Data.ByteString.Lazy as BSLazy
+import qualified Data.ByteString.Lazy.UTF8 as BSLazy
 import qualified Data.Text as T
 import Data.Text.Encoding
 
@@ -19,14 +20,29 @@ strip = encodeUtf8 . T.strip . decodeUtf8
 stio :: [BS.ByteString] -> IO [BSLazy.ByteString]
 stio = return . map BSLazy.fromStrict
 
+strio :: [String] -> IO [BSLazy.ByteString]
+strio = return . map BSLazy.fromString
+
 st :: [BS.ByteString] -> [BSLazy.ByteString]
 st = map BSLazy.fromStrict
 
+botlist :: [BS.ByteString]
+botlist = map BS.fromString [
+    "김젼봇",
+    "lambdaChan"]
+
+
 parse :: BS.ByteString -> BS.ByteString -> IO [BSLazy.ByteString]
 parse str nick
-    | cmd [q|:hello|] || cmd [q|!hello|] = stio [[qq|Hello, $nick!|]]
-    | cmd [q|:dic |] || cmd [q|!dic |] = DaumDic.parse (strip $ BS.drop 5 str)
-    | cmd [q|:latex |] || cmd [q|!latex |] = stio [[q|http://latex.codecogs.com/png.latex|] `BS.append` renderQuery True [((strip $ BS.drop 7 str), Nothing)]]
+    | cmd ":hello" || cmd "!hello" = stio [[qq|Hello, $nick!|]]
+    | cmd ":dic " || cmd "!dic " = DaumDic.parse (strip $ BS.drop 5 str)
+    | cmd ":latex " || cmd "!latex " = stio [[q|http://latex.codecogs.com/png.latex|] `BS.append` renderQuery True [((strip $ BS.drop 7 str), Nothing)]]
+    | exactly "d(ㅇㅅㅇ" && isHuman = strio ["ㅇㅅㅇ)b "]
+    | exactly "ㅇㅅ<" && isHuman = strio [">ㅅㅇ "]
+    | exactly "/ㅇㅅㅇ)/" && isHuman = strio ["\\(ㅇㅅㅇ\\ "]
+    | exactly "\\(ㅇㅅㅇ\\" && isHuman = strio ["/ㅇㅅㅇ)/ "]
     | otherwise = return []
-    where cmd = (`BS.isPrefixOf` str)
+    where cmd = (`BS.isPrefixOf` str) . BS.fromString
+          exactly = (str ==) . BS.fromString
+          isHuman = nick `notElem` botlist
 
